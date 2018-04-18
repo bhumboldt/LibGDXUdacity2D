3efc1aa70687b229128d2205ebb7a7d2c9f5d512
Brandon Humboldt <bhumboldt@wisc.edu>
2018-04-17 20:41:25 -0500

    Initial commite

---
+++ android/src/com/udacity/course1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.udacity.course1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.udacity.course1.UdacityCourse1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UdacityCourse1(), config);
+	}
+}
